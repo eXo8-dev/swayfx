3d7358c607f4908563b1c85da102c3e1798c6710
eXo8_ <128096384+eXo8-dev@users.noreply.github.com>
2024-07-06 10:53:33 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,12 @@ sudo dnf copr enable -y g3tchoo/prismlauncher
 sudo dnf install -y swayfx waybar alacritty git wofi grim slurp firefox neofetch meson pulseaudio pavucontrol telegram-desktop xdg-desktop-portal-wlr imv mpv grim slurp swaybg swaylock mako tar htop unzip cava pcmanfm wf-recorder polkit-gnome wl-clipboard webkitgtk6.0 qt5-qtwebview qt6-qtwebview wxGTK-webview prismlauncher
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
-mkdir ~./config/waybar
+
+mkdir my_config/
+mv swayfx/my_config/* /my_config/
+
+mkdir .config/waybar/
+mv my_config/waybar/* .config/waybar/
+
+mkdir .config/neofetch/
+mv my_config/neofetch/* .config/neofetch/
